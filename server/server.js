69971a3cc994983c8501dd7dae59781496452e33
Teruk0 <mrodrigoh@yahoo.com>
2023-10-14 14:47:41 -0700

    Setup the front + backend
    the front end displays a simple list of three users that it fetches from the backend using an API call

---
+++ server/server.js
@@ -0,0 +1,8 @@
+import express from 'express'
+const app = express()
+
+app.get("/api", (req,res) => {
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
+})
+
+app.listen(5000, () => {console.log("server started on port 5000")})
